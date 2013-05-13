147043b42bf721b4722e7235161b4cfb15f44faa
Dave Shepherd <dave.shepherd@endor.me.uk>
2013-05-13 17:17:57 +0100

    added some additional metrics

--- src/test/java/com/firstutility/labs/metrics/StatsdClientTest.java
+++ src/test/java/com/firstutility/labs/metrics/StatsdClientTest.java
@@ -2,17 +2,23 @@ package com.firstutility.labs.metrics;
 
 import java.io.IOException;
 import java.net.UnknownHostException;
+import java.util.Random;
 
 import org.junit.Test;
 
 public class StatsdClientTest {
 
 	@Test
-	public void test() throws UnknownHostException, IOException {
+	public void test() throws UnknownHostException, IOException, InterruptedException {
 
 		StatsdClient client = new StatsdClient("10.11.12.53", 8125);
 		// increment by 1
-		client.increment("foo.bar.baz");
+		
+		for (int i=0;i<60; i++) {
+			client.increment("foo.bar.baz");
+			Thread.sleep(1000l);
+		}
+		
 		// increment by 10
 		client.increment("foo.bar.baz", 10);
 		// sample rate
@@ -24,6 +30,12 @@ public class StatsdClientTest {
 		// multiple keys with a sample rate
 		client.increment(10, .1, "foo.bar.baz", "foo.bar.boo", "foo.baz.bar");
 
+		int timerValue = Math.abs(new Random().nextInt() % 1000);
+		client.timing("foo.bar.time", timerValue);
+
+		int gaugeValue = Math.abs(new Random().nextInt() % 100);
+		client.gauge("foo.bar.gauge", gaugeValue);
+		
 		// To enable multi metrics (aka more than 1 metric in a UDP packet)
 		// (disabled by default)
 		client.enableMultiMetrics(true); // disable by passing in false
